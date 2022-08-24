e8039a0640dd1770ac38a8789efdf54a1e4b4cd0
akbaryopiananda <yopiananda.akbar@gmail.com>
2022-08-24 17:10:15 +0700

    Register v.2

--- pages/auth/register.tsx
+++ pages/auth/register.tsx
@@ -1,16 +1,6 @@
 
 import Head from 'next/head';
-import {
-    Button,
-    Grid,
-    GridItem,
-    VStack,
-    Box,
-    Heading,
-    Text,
-    Input,
-    Flex
-} from '@chakra-ui/react';
+import {Grid} from '@chakra-ui/react';
 
 import LayoutAuth from '../../components/auth/layout';
 import FormRegister from '../../components/auth/form-register';
@@ -22,10 +12,7 @@ const Register = () => {
                 <title>Register</title>
             </Head>
     
-            <Grid
-                bg='#F8F8FB'
-                templateColumns={{ base: '1fr', md: '1fr 1fr' }}
-            >
+            <Grid bg='#F8F8FB' templateColumns={{ base: '1fr', md: '1fr 1fr' }}>
                 <LayoutAuth />
                 <FormRegister />
             </Grid>
