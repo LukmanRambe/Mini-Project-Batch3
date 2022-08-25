758ab01313bb1fc66c8579b257f4de6be4bb2c6c
akbaryopiananda <yopiananda.akbar@gmail.com>
2022-08-25 21:58:57 +0700

    Jam calendar slicing done

--- components/home/Layout/CalendarClock.tsx
+++ components/home/Layout/CalendarClock.tsx
@@ -1,24 +1,23 @@
 // Styles
-import { Grid, GridItem } from '@chakra-ui/react';
+import { Box, Center, Flex, Grid, GridItem, Heading, Spacer, Text, VStack } from '@chakra-ui/react';
 import { Calendar } from '@natscale/react-calendar';
 import '@natscale/react-calendar/dist/main.css';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback, SetStateAction } from 'react';
 import AnalogueClock from 'react-analogue-clock';
-import {Text} from "@chakra-ui/react";
-import { Container } from '@chakra-ui/react'
+
 
 const CalendarClock = () => {
-	const [value, setValue] = useState(new Date());
+	const isDisabled = useCallback((date: { getDate: () => number; getMonth: () => number; }) => {
+	// disable wednesdays and any date that is divisible by 5
+	if (date.getDate() == 17) {
+		if(date.getMonth() == 7){
+			return true;
+		}
+	}
+	}, []);
 	const time = new Date();
 	const timerun = time.toLocaleString('en-US', { hour: 'numeric', hour12: true, minute:'numeric' });
-	  useEffect(() => {
-		const interval = setInterval(() => setValue(new Date()), 1000);
-	
-		return () => {
-		  clearInterval(interval);
-		};
-	  }, []);
-	  const clockOptions = {
+	const clockOptions = {
 		baseColor: '#22345E',
 		borderColor: '#15264F',
 		borderWidth: 5,
@@ -31,22 +30,60 @@ const CalendarClock = () => {
 		notchColor: '#737373',
 		numbersColor: '#000000',
 		showNumbers: false,
-		size: 150
+		size: 150,
 	}
 	return (
-		<Container>
-			<Grid templateColumns='repeat(4, 1fr)' gap={4}>
-				<GridItem colSpan={2}>
+		<Box m="20px" border="1px" w="100%" borderColor="gray.200" >
+			<Grid justifyContent='center'
+                alignItems='center'
+				borderBottom="1px"
+				borderColor="gray.200"
+				h="5rem"
+				w="100%">
+				<Heading as="h2" size="md">
+					Jam dan Tanggal Hari ini
+				</Heading>
+			</Grid>
+			<Grid justifyContent='center'
+                alignItems='center' gap={5}>
+				<GridItem>
 					<AnalogueClock {...clockOptions} />
 				</GridItem>
-				<GridItem colSpan={2}>
-					<Text fontSize='4xl'>{timerun}</Text>
+				<GridItem>
+					<Heading>{timerun}</Heading>
 				</GridItem>
 			</Grid>
-			<Grid>
-				<Calendar value={value} />
+			<Grid justifyContent='center'
+                alignItems='center' gap={5} padding-bottom={5}>
+				<GridItem>
+					<Calendar size={250} fontSize={14} hideAdjacentDates isDisabled={isDisabled} />
+				</GridItem>
+				<h3>Keterangan</h3>
+				<VStack align='stretch'>
+					<Box bg='red' borderRadius={16} padding={4} maxWidth={400}>
+					<Flex>
+								<Center>
+						<Box padding={2} className='segitiga'>
+							<Heading size='md'>
+								17
+							</Heading>
+						</Box>
+						<Box className='segitiga2'>
+
+						</Box>
+								</Center>
+						<Box className='borderLeft'>
+							
+						</Box>
+						<Box>
+							<Text color='white'>Rabu, 17 Agustus 2022</Text>
+							<Text color='white' fontSize='xs'>Hari Proklamasi Kemerdekaan RI</Text>
+						</Box>
+					</Flex>
+					</Box>
+				</VStack>
 			</Grid>
-		</Container>
+		</Box>
 	)
 }
 
