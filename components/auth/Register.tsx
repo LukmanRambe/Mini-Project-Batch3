45e2ce8de0ef6676da85079bd6a705394d6d08dc
akbaryopiananda <yopiananda.akbar@gmail.com>
2022-08-26 11:01:00 +0700

    update slicing login/register

--- components/auth/Register.tsx
+++ components/auth/Register.tsx
@@ -18,14 +18,34 @@ import {
 import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
 import NextLink from "next/link";
 
-const Login = () => {
+const FormRegister = () => {
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [ulangiPassword, setUlangiPassword] = useState("");
 
   const [show, setShow] = useState<boolean>(false);
+  const [showUlangi, setShowUlangi] = useState<boolean>(false);
 
   const handleShow = () => {
     setShow(!show);
+  };
+  const handleShowUlangi = () => {
+    setShowUlangi(!showUlangi);
+  };
+  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  };
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+  const handleUlangiPasswordChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setUlangiPassword(event.target.value);
   };
   return (
     <GridItem>
@@ -43,6 +63,24 @@ const Login = () => {
           </Box>
           <form action="submit">
             <FormControl isRequired>
+              <Flex marginTop={4} marginBottom={2}>
+                <Text fontSize="md">Nama </Text>
+                <Text fontSize="md" color="red">
+                  {" "}
+                  *
+                </Text>
+              </Flex>
+              <Input
+                type="text"
+                py="13px"
+                px="20px"
+                borderRadius="12px"
+                bg="white"
+                value={name}
+                onChange={(e) => handleNameChange(e)}
+                placeholder="Masukkan Nama"
+                required
+              />
               <Flex gap={1} marginTop={4} marginBottom={2}>
                 <Text fontSize="md">Email </Text>
                 <Text fontSize="md" color="red">
@@ -57,6 +95,8 @@ const Login = () => {
                 borderRadius="12px"
                 bg="white"
                 placeholder="Masukkan Email"
+                value={email}
+                onChange={(e) => handleEmailChange(e)}
                 required
               />
               <Flex gap={1} marginTop={4} marginBottom={2}>
@@ -73,6 +113,8 @@ const Login = () => {
                   px="20px"
                   borderRadius="12px"
                   bg="white"
+                  value={password}
+                  onChange={(e) => handlePasswordChange(e)}
                   placeholder="Password"
                   required
                 />
@@ -88,11 +130,38 @@ const Login = () => {
                   </Button>
                 </InputRightElement>
               </InputGroup>
-              <Flex justify="flex-end" mt="15px">
-                <NextLink href="/auth/forgot-password" passHref>
-                  <Link color="primary">Lupa Password?</Link>
-                </NextLink>
+              <Flex gap={1} marginTop={4} marginBottom={2}>
+                <Text fontSize="md">Ulangi Password</Text>
+                <Text fontSize="md" color="red">
+                  {" "}
+                  *
+                </Text>
               </Flex>
+              <InputGroup size="md">
+                <Input
+                  type={showUlangi ? "text" : "password"}
+                  name="ulangiPassword"
+                  py="13px"
+                  px="20px"
+                  borderRadius="12px"
+                  bg="white"
+                  value={ulangiPassword}
+                  onChange={(e) => handleUlangiPasswordChange(e)}
+                  placeholder="Ulangi Password"
+                  required
+                />
+                <InputRightElement>
+                  <Button
+                    h="full"
+                    bg="transparent"
+                    size="md"
+                    borderRightRadius="12px"
+                    borderLeftRadius="0"
+                    onClick={handleShowUlangi}>
+                    {showUlangi ? <ViewIcon /> : <ViewOffIcon />}
+                  </Button>
+                </InputRightElement>
+              </InputGroup>
               <Button
                 size="md"
                 bg="#BA181B"
@@ -111,10 +180,10 @@ const Login = () => {
           <Center>
             <Flex mt="20px">
               <Text color="#737373" fontSize="sm">
-                Belum Punya Akun?
-                <NextLink href="/auth/register" passHref>
+                Sudah Punya Akun?
+                <NextLink href="/" passHref>
                   <Link color="primary" fontWeight="bold" ml="1">
-                    Daftar
+                    Masuk
                   </Link>
                 </NextLink>
               </Text>
@@ -126,5 +195,4 @@ const Login = () => {
   );
 };
 
-export default Login;
-
+export default FormRegister;
