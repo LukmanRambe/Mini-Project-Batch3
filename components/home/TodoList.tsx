74fb29e742acc09cab7d302afe1997a7aea7b2ec
Lukman Rambe <lukmanrambe1945@gmail.com>
2022-09-01 12:07:11 +0700

    Add undo done task feature

--- components/home/TodoList.tsx
+++ components/home/TodoList.tsx
@@ -1,210 +1,246 @@
-import { useState } from "react";
-import axios from "axios";
-import moment from "moment";
-import { Todo } from "../../ts/interface";
-import { serviceURL } from "../../utils/config";
-import { mutate } from "swr";
-import DeleteModal from "./Modal/DeleteModal";
+import { useState } from 'react'
+import axios from 'axios'
+import moment from 'moment'
+import { Todo } from '../../ts/interface'
+import { serviceURL } from '../../utils/config'
+import { mutate } from 'swr'
+import DeleteModal from './Modal/DeleteModal'
 
 // Styles
-import { Box, Flex, Text, Button, useToast } from "@chakra-ui/react";
+import { Box, Flex, Text, Button, useToast } from '@chakra-ui/react'
 
 interface Props {
-  todos: Todo[];
+	todos: Todo[]
 }
 
 const TodoList: React.FC<Props> = ({ todos }: Props) => {
-  const toast = useToast();
-  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+	const toast = useToast()
+	const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false)
 
-  const updateTaskStatus = async (taskId: number | undefined) => {
-    if (taskId) {
-      await axios
-        .post(`https://nouky.xyz/b3/task/finish_task/${taskId}`, null, {
-          headers: {
-            Authorization: "Bearer " + localStorage.getItem("xtoken"),
-          },
-        })
-        .then((result) => {
-          toast({
-            position: "top",
-            title: "Berhasil",
-            description: result.data.message,
-            status: "success",
-            duration: 3000,
-            isClosable: true,
-          });
-          mutate(`${serviceURL}/task/show_todo`);
-        })
-        .catch((err) => {
-          if (err.code === "ECONNABORTED") {
-            toast({
-              position: "top",
-              title: "Gagal",
-              description:
-                "Tidak dapat menjangkau Server, Periksa koneksi anda dan ulangi beberapa saat lagi.",
-              status: "error",
-              duration: 3000,
-              isClosable: true,
-            });
-          } else if (err.response) {
-            toast({
-              position: "top",
-              title: "Gagal",
-              description: err.message,
-              status: "error",
-              duration: 3000,
-              isClosable: true,
-            });
-          }
-        });
-    }
-  };
+	const updateTaskStatus = async (taskId: number | undefined) => {
+		const todoData = todos.filter(todo => todo.id_task === taskId)[0]
+		if (todoData) {
+			if (todoData.keterangan === 'Progress') {
+				await axios
+					.post(`https://nouky.xyz/b3/task/finish_task/${taskId}`, null, {
+						headers: {
+							Authorization: 'Bearer ' + localStorage.getItem('xtoken')
+						}
+					})
+					.then(result => {
+						toast({
+							position: 'top',
+							title: 'Berhasil',
+							description: result.data.message,
+							status: 'success',
+							duration: 3000,
+							isClosable: true
+						})
+						mutate(`${serviceURL}/task/show_all`)
+						mutate(`${serviceURL}/task/show_done`)
+						mutate(`${serviceURL}/task/show_overdue`)
+					})
+					.catch(err => {
+						if (err.code === 'ECONNABORTED') {
+							toast({
+								position: 'top',
+								title: 'Gagal',
+								description:
+									'Tidak dapat menjangkau Server, Periksa koneksi anda dan ulangi beberapa saat lagi.',
+								status: 'error',
+								duration: 3000,
+								isClosable: true
+							})
+						} else if (err.response) {
+							toast({
+								position: 'top',
+								title: 'Gagal',
+								description: err.message,
+								status: 'error',
+								duration: 3000,
+								isClosable: true
+							})
+						}
+					})
+			} else if (todoData.keterangan === 'Done') {
+				await axios
+					.post(`https://nouky.xyz/b3/task/undo_done_task/${taskId}`, null, {
+						headers: {
+							Authorization: 'Bearer ' + localStorage.getItem('xtoken')
+						}
+					})
+					.then(result => {
+						toast({
+							position: 'top',
+							title: 'Berhasil',
+							description: result.data.message,
+							status: 'success',
+							duration: 3000,
+							isClosable: true
+						})
+						mutate(`${serviceURL}/task/show_all`)
+						mutate(`${serviceURL}/task/show_done`)
+						mutate(`${serviceURL}/task/show_overdue`)
+					})
+					.catch(err => {
+						if (err.code === 'ECONNABORTED') {
+							toast({
+								position: 'top',
+								title: 'Gagal',
+								description:
+									'Tidak dapat menjangkau Server, Periksa koneksi anda dan ulangi beberapa saat lagi.',
+								status: 'error',
+								duration: 3000,
+								isClosable: true
+							})
+						} else if (err.response) {
+							toast({
+								position: 'top',
+								title: 'Gagal',
+								description: err.message,
+								status: 'error',
+								duration: 3000,
+								isClosable: true
+							})
+						}
+					})
+			}
+		}
+	}
 
-  return (
-    <div>
-      {todos?.map((todo, index) => (
-        <Box boxShadow="rgb(173 173 173 / 10%) 0px 6px 0px 0px" key={index}>
-          <Flex direction="row" justify="space-between" w="full" align="center">
-            <Flex
-              align="center"
-              opacity={`${todo.keterangan === "Done" ? "0.6" : ""}`}
-            >
-              <Box
-                _hover={{
-                  color: `${todo.keterangan !== "Done" ? "primary" : ""}`,
-                  cursor: "pointer",
-                }}
-                _checked={{ color: "primary" }}
-                onClick={() => updateTaskStatus(todo.id_task)}
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill={`${todo.keterangan === "Done" ? "#C53030" : "none"}`}
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <rect
-                    x="3"
-                    y="3"
-                    color={`${todo.keterangan === "Done" ? "#C53030" : ""}`}
-                    width="18"
-                    height="18"
-                    rx="4"
-                  ></rect>
-                  <path
-                    color={`${todo.keterangan === "Done" ? "white" : ""}`}
-                    d="M9 12l2.25 2L15 10"
-                  ></path>
-                </svg>
-              </Box>
-              <Box w="full" m={4}>
-                <Text
-                  fontSize="md"
-                  fontWeight="bold"
-                  color="primary"
-                  style={{
-                    textDecoration: `${
-                      todo.keterangan === "Done" ? "line-through" : ""
-                    }`,
-                  }}
-                >
-                  {todo.judul}
-                </Text>
-                <Text
-                  fontSize="sm"
-                  w="400px"
-                  textOverflow="ellipsis"
-                  whiteSpace="nowrap"
-                  textDecoration="initial"
-                  overflow="hidden"
-                  style={{
-                    textDecoration: `${
-                      todo.keterangan === "Done" ? "line-through" : ""
-                    }`,
-                  }}
-                >
-                  {todo.komentar}
-                </Text>
-                <Flex align="center">
-                  <Text color="red" mr="2" fontSize="sm">
-                    {todo.jam}
-                  </Text>
-                  <Text mr={2} fontSize="sm">
-                    -{" "}
-                    {moment(todo.tanggal)
-                      .locale("id")
-                      .format("dddd, DD MMMM YYYY")}
-                  </Text>
-                </Flex>
-              </Box>
-            </Flex>
-            <Flex>
-              <Button
-                mr={2}
-                bg="transparent"
-                color="green"
-                _hover={{ background: "gray.100" }}
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <path d="M16.474 5.408l2.118 2.117m-.756-3.982L12.109 9.27a2.118 2.118 0 0 0-.58 1.082L11 13l2.648-.53c.41-.082.786-.283 1.082-.579l5.727-5.727a1.853 1.853 0 1 0-2.621-2.621z"></path>
-                  <path d="M19 15v3a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V7a2 2 0 0 1 2-2h3"></path>
-                </svg>
-              </Button>
-              <Button
-                bg="transparent"
-                color="primary"
-                _hover={{ background: "gray.100" }}
-                onClick={() => setIsDeleteModalOpen((prevState) => !prevState)}
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <path d="M4 6h16l-1.58 14.22A2 2 0 0 1 16.432 22H7.568a2 2 0 0 1-1.988-1.78L4 6z"></path>
-                  <path d="M7.345 3.147A2 2 0 0 1 9.154 2h5.692a2 2 0 0 1 1.81 1.147L18 6H6l1.345-2.853z"></path>
-                  <path d="M2 6h20"></path>
-                  <path d="M10 11v5"></path>
-                  <path d="M14 11v5"></path>
-                </svg>
-              </Button>
-            </Flex>
-          </Flex>
+	return (
+		<div>
+			{todos?.map((todo, index) => (
+				<Box boxShadow='rgb(173 173 173 / 10%) 0px 6px 0px 0px' key={index}>
+					<Flex direction='row' justify='space-between' w='full' align='center'>
+						<Flex
+							align='center'
+							opacity={`${todo.keterangan === 'Done' ? '0.6' : ''}`}>
+							<Box
+								_hover={{
+									color: `${todo.keterangan !== 'Done' ? 'primary' : ''}`,
+									cursor: 'pointer'
+								}}
+								_checked={{ color: 'primary' }}
+								onClick={() => updateTaskStatus(todo.id_task)}>
+								<svg
+									xmlns='http://www.w3.org/2000/svg'
+									width='24'
+									height='24'
+									viewBox='0 0 24 24'
+									fill={`${todo.keterangan === 'Done' ? '#C53030' : 'none'}`}
+									stroke='currentColor'
+									strokeWidth='2'
+									strokeLinecap='round'
+									strokeLinejoin='round'>
+									<rect
+										x='3'
+										y='3'
+										color={`${todo.keterangan === 'Done' ? '#C53030' : ''}`}
+										width='18'
+										height='18'
+										rx='4'></rect>
+									<path
+										color={`${todo.keterangan === 'Done' ? 'white' : ''}`}
+										d='M9 12l2.25 2L15 10'></path>
+								</svg>
+							</Box>
+							<Box w='full' m={4}>
+								<Text
+									fontSize='md'
+									fontWeight='bold'
+									color='primary'
+									style={{
+										textDecoration: `${
+											todo.keterangan === 'Done' ? 'line-through' : ''
+										}`
+									}}>
+									{todo.judul}
+								</Text>
+								<Text
+									fontSize='sm'
+									w='400px'
+									textOverflow='ellipsis'
+									whiteSpace='nowrap'
+									textDecoration='initial'
+									overflow='hidden'
+									style={{
+										textDecoration: `${
+											todo.keterangan === 'Done' ? 'line-through' : ''
+										}`
+									}}>
+									{todo.komentar}
+								</Text>
+								<Flex align='center'>
+									<Text color='red' mr='2' fontSize='sm'>
+										{todo.jam}
+									</Text>
+									<Text mr={2} fontSize='sm'>
+										-{' '}
+										{moment(todo.tanggal)
+											.locale('id')
+											.format('dddd, DD MMMM YYYY')}
+									</Text>
+								</Flex>
+							</Box>
+						</Flex>
+						<Flex>
+							<Button
+								mr={2}
+								bg='transparent'
+								color='green'
+								_hover={{ background: 'gray.100' }}>
+								<svg
+									xmlns='http://www.w3.org/2000/svg'
+									width='24'
+									height='24'
+									viewBox='0 0 24 24'
+									fill='none'
+									stroke='currentColor'
+									strokeWidth='2'
+									strokeLinecap='round'
+									strokeLinejoin='round'>
+									<path d='M16.474 5.408l2.118 2.117m-.756-3.982L12.109 9.27a2.118 2.118 0 0 0-.58 1.082L11 13l2.648-.53c.41-.082.786-.283 1.082-.579l5.727-5.727a1.853 1.853 0 1 0-2.621-2.621z'></path>
+									<path d='M19 15v3a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V7a2 2 0 0 1 2-2h3'></path>
+								</svg>
+							</Button>
+							<Button
+								bg='transparent'
+								color='primary'
+								_hover={{ background: 'gray.100' }}
+								onClick={() => setIsDeleteModalOpen(prevState => !prevState)}>
+								<svg
+									xmlns='http://www.w3.org/2000/svg'
+									width='24'
+									height='24'
+									viewBox='0 0 24 24'
+									fill='none'
+									stroke='currentColor'
+									strokeWidth='2'
+									strokeLinecap='round'
+									strokeLinejoin='round'>
+									<path d='M4 6h16l-1.58 14.22A2 2 0 0 1 16.432 22H7.568a2 2 0 0 1-1.988-1.78L4 6z'></path>
+									<path d='M7.345 3.147A2 2 0 0 1 9.154 2h5.692a2 2 0 0 1 1.81 1.147L18 6H6l1.345-2.853z'></path>
+									<path d='M2 6h20'></path>
+									<path d='M10 11v5'></path>
+									<path d='M14 11v5'></path>
+								</svg>
+							</Button>
+						</Flex>
+					</Flex>
 
-          <DeleteModal
-            id_task={todo.id_task}
-            isDeleteModalOpen={isDeleteModalOpen}
-            setIsDeleteModalOpen={setIsDeleteModalOpen}
-            onDeleteModalClose={() =>
-              setIsDeleteModalOpen((prevState) => !prevState)
-            }
-          />
-        </Box>
-      ))}
-    </div>
-  );
-};
+					<DeleteModal
+						id_task={todo.id_task}
+						isDeleteModalOpen={isDeleteModalOpen}
+						setIsDeleteModalOpen={setIsDeleteModalOpen}
+						onDeleteModalClose={() =>
+							setIsDeleteModalOpen(prevState => !prevState)
+						}
+					/>
+				</Box>
+			))}
+		</div>
+	)
+}
 
-export default TodoList;
+export default TodoList
--- components/home/Todos.tsx
+++ components/home/Todos.tsx
@@ -1,83 +1,83 @@
-import React, { useState, useEffect } from "react";
-import { useRouter } from "next/router";
-import { Box, Flex, Heading, Text, Button, useToast } from "@chakra-ui/react";
-import styles from "../../styles/Home.module.css";
-import TodoList from "../../components/home/TodoList";
-import AddModal from "./Modal/AddModal";
-import { useTodo } from "../../hooks/remote/useTodo";
+import React, { useState } from 'react'
+import { useRouter } from 'next/router'
+import { Box, Flex, Heading, Text, Button, useToast } from '@chakra-ui/react'
+import styles from '../../styles/Home.module.css'
+import TodoList from '../../components/home/TodoList'
+import AddModal from './Modal/AddModal'
+import { useTodo } from '../../hooks/remote/useTodo'
 
 const Todos = ({ Header }: any) => {
-  const router = useRouter();
-  const toast = useToast();
-  const pathName = router.asPath;
-  const { todos, errorTodos } = useTodo(
-    pathName === "/home"
-      ? "show_todo"
-      : pathName === "/done"
-      ? "show_done"
-      : pathName === "/overdue"
-      ? "show_overdue"
-      : ""
-  );
-  const [visibleAddModal, setVisibleAddModal] = useState(false);
+	const router = useRouter()
+	const toast = useToast()
+	const pathName = router.asPath
+	const { todos, errorTodos } = useTodo(
+		pathName === '/home'
+			? 'show_all'
+			: pathName === '/done'
+			? 'show_done'
+			: pathName === '/overdue'
+			? 'show_overdue'
+			: ''
+	)
+	const [visibleAddModal, setVisibleAddModal] = useState(false)
 
-  if (errorTodos) {
-    toast({
-      title: errorTodos.message,
-      status: "error",
-      duration: 9000,
-      isClosable: true,
-    });
-  }
+	if (errorTodos) {
+		toast({
+			title: errorTodos.message,
+			status: 'error',
+			duration: 9000,
+			isClosable: true
+		})
+	}
 
-  return (
-    <Box
-      flex="3"
-      m={{ base: "0px", xl: "20px" }}
-      border="1px"
-      w="full"
-      borderColor="gray.200">
-      <Flex
-        px="28px"
-        align="center"
-        h="5rem"
-        w="100%"
-        mb="30px"
-        justify="space-between"
-        borderBottom="1px"
-        borderColor="gray.200">
-        <Heading as="h2" size="md">
-          {Header}
-        </Heading>
-        {pathName === "/home" ? (
-          <Button onClick={() => setVisibleAddModal(true)}>Tambah</Button>
-        ) : (
-          ""
-        )}
-      </Flex>
-      <Flex
-        direction="column"
-        overflowY="auto"
-        px="28px"
-        pb="28px"
-        h={{ base: "500px", xl: "780px" }}
-        className={styles.todo}>
-        {/* Todos Done */}
-        {todos?.data?.length != 0 ? (
-          <TodoList todos={todos?.data} />
-        ) : (
-          <Text textAlign="center" mb="25px">
-            Belum ada Tugas untuk dilakukan
-          </Text>
-        )}
-      </Flex>
-      {/* add Todos */}
-      <AddModal
-        isOpen={visibleAddModal}
-        onClose={() => setVisibleAddModal((prev) => !prev)}
-      />
-    </Box>
-  );
-};
+	return (
+		<Box
+			flex='3'
+			m={{ base: '0px', xl: '20px' }}
+			border='1px'
+			w='full'
+			borderColor='gray.200'>
+			<Flex
+				px='28px'
+				align='center'
+				h='5rem'
+				w='100%'
+				mb='30px'
+				justify='space-between'
+				borderBottom='1px'
+				borderColor='gray.200'>
+				<Heading as='h2' size='md'>
+					{Header}
+				</Heading>
+				{pathName === '/home' ? (
+					<Button onClick={() => setVisibleAddModal(true)}>Tambah</Button>
+				) : (
+					''
+				)}
+			</Flex>
+			<Flex
+				direction='column'
+				overflowY='auto'
+				px='28px'
+				pb='28px'
+				h={{ base: '500px', xl: '780px' }}
+				className={styles.todo}>
+				{/* Todos Done */}
+				{todos?.data?.length != 0 ? (
+					<TodoList todos={todos?.data} />
+				) : (
+					<Text textAlign='center' mb='25px'>
+						Belum ada Tugas untuk dilakukan
+					</Text>
+				)}
+			</Flex>
+			{/* add Todos */}
+			<AddModal
+				isOpen={visibleAddModal}
+				onClose={() => setVisibleAddModal(prev => !prev)}
+			/>
+		</Box>
+	)
+}
 
-export default Todos;
+export default Todos
