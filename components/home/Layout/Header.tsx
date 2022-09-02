f014c2fdbe87a65c8e2831f9939fdde54bc392d0
Lukman Rambe <lukmanrambe1945@gmail.com>
2022-09-02 16:58:56 +0700

    Add sidebar responsive

--- components/home/Layout/Header.tsx
+++ components/home/Layout/Header.tsx
@@ -1,164 +1,200 @@
 // Styles
-import { Container, Flex, Stack, Box, Heading, Text, useToast } from "@chakra-ui/react";
-import { useCountDoneTodo } from "../../../hooks/remote/useCountDoneTodo";
-import { useCountOverdueTodo } from "../../../hooks/remote/useCountOverdueTodo";
-import { useCountTodo } from "../../../hooks/remote/useCountTodo";
+import {
+	Container,
+	Flex,
+	Stack,
+	Box,
+	Heading,
+	Text,
+	useToast
+} from '@chakra-ui/react'
+import { useCountDoneTodo } from '../../../hooks/remote/useCountDoneTodo'
+import { useCountOverdueTodo } from '../../../hooks/remote/useCountOverdueTodo'
+import { useCountTodo } from '../../../hooks/remote/useCountTodo'
 
 const Header = () => {
-  const { countTodo, errorCountTodo } = useCountTodo()
-  const { countDoneTodo, errorCountDoneTodo } = useCountDoneTodo()
-  const { countOverdueTodo, errorCountOverdueTodo } = useCountOverdueTodo()
-  const toast = useToast()
+	const { countTodo, errorCountTodo } = useCountTodo()
+	const { countDoneTodo, errorCountDoneTodo } = useCountDoneTodo()
+	const { countOverdueTodo, errorCountOverdueTodo } = useCountOverdueTodo()
+	const toast = useToast()
 
-  if (errorCountTodo) {
-    toast({
-      title: errorCountTodo.message,
-      status: 'error',
-      duration: 9000,
-      isClosable: true
-    })
-  }
+	if (errorCountTodo) {
+		toast({
+			title: errorCountTodo.message,
+			status: 'error',
+			duration: 9000,
+			isClosable: true
+		})
+	}
 
-  if (errorCountDoneTodo) {
-    toast({
-      title: errorCountDoneTodo.message,
-      status: 'error',
-      duration: 9000,
-      isClosable: true
-    })
-  }
+	if (errorCountDoneTodo) {
+		toast({
+			title: errorCountDoneTodo.message,
+			status: 'error',
+			duration: 9000,
+			isClosable: true
+		})
+	}
 
-  if (errorCountOverdueTodo) {
-    toast({
-      title: errorCountOverdueTodo.message,
-      status: 'error',
-      duration: 9000,
-      isClosable: true
-    })
-  }
+	if (errorCountOverdueTodo) {
+		toast({
+			title: errorCountOverdueTodo.message,
+			status: 'error',
+			duration: 9000,
+			isClosable: true
+		})
+	}
 
-  return (
-    <Box borderBottom="1px" borderBottomColor="#EFEFEF" p="40px" pb="60px">
-      <Flex
-        justify={{
-          base: "center",
-          md: "center",
-          lg: "space-between"
-        }}
-        flexWrap="wrap"
-        gap={4}
+	return (
+		<Box borderBottom='1px' borderBottomColor='#EFEFEF' p='40px' pb='60px'>
+			<Flex
+				justify={{
+					base: 'center',
+					md: 'center',
+					lg: 'space-between'
+				}}
+				flexWrap='wrap'
+				gap={4}>
+				<Flex gap='15px' align='center'>
+					{/* START: Profile Picture */}
+					<Box
+						color='#ffffff'
+						bg='#718096'
+						borderRadius='100%'
+						w='82px'
+						h='82px'
+						display='flex'
+						alignItems='center'
+						justifyContent='center'>
+						<Heading fontSize='48px' fontWeight='700'>
+							J
+						</Heading>
+					</Box>
+					{/* END: Profile Picture */}
 
-      >
-        <Flex gap="15px" align="center">
-          {/* START: Profile Picture */}
-          <Box
-            color="#ffffff"
-            bg="#718096"
-            borderRadius="100%"
-            w="82px"
-            h="82px"
-            display="flex"
-            alignItems="center"
-            justifyContent="center">
-            <Heading fontSize="48px" fontWeight="700">
-              J
-            </Heading>
-          </Box>
-          {/* END: Profile Picture */}
+					{/* START: Username & Typography */}
+					<Box>
+						<Heading fontSize='24px' fontWeight='700'>
+							John Doe
+						</Heading>
+						<Text fontSize='18px' color='#737373'>
+							Ayo lebih produktif
+						</Text>
+					</Box>
+					{/* END: Username & Typography */}
+				</Flex>
 
-          {/* START: Username & Typography */}
-          <Box>
-            <Heading fontSize="24px" fontWeight="700">
-              John Doe
-            </Heading>
-            <Text fontSize="18px" color="#737373">
-              Ayo lebih produktif
-            </Text>
-          </Box>
-          {/* END: Username & Typography */}
-        </Flex>
+				{/* START: Todos Counter */}
+				<Stack
+					w='100%'
+					direction={['column', 'row', 'row']}
+					gap={{
+						base: 5,
+						lg: 10
+					}}
+					mt={{
+						base: 10,
+						md: 10,
+						lg: 0
+					}}>
+					{/* START: Todo Counter */}
+					<Container
+						display='flex'
+						alignItems='center'
+						justifyContent='center'
+						color='#ffffff'
+						bgImage='https://mini-project-vocasia.vercel.app/dashboard/todo-stats-bg/todo-counter-bg.svg'
+						borderRadius={{
+							base: '12px',
+							lg: '6px'
+						}}
+						w={{
+							base: '100%',
+							lg: '140px',
+							sm: '100%'
+						}}
+						h={{
+							base: '100%',
+							lg: '77px',
+							sm: '90px'
+						}}
+						bgSize='cover'
+						bgRepeat='no-repeat'>
+						<Heading fontSize='47px' fontWeight='400' mr='8px'>
+							{countTodo?.jumlah_data}
+						</Heading>
+						<Text fontSize='12px'>
+							To Do <br /> Task Today
+						</Text>
+					</Container>
+					{/* START: Todo Counter */}
 
-        {/* START: Todos Counter */}
-        <Stack
-          direction={['column', 'row', 'row']}
-          gap={{
-            base: 5,
-            lg: 10
-          }}
-          mt={{
-            base: 10,
-            md: 10,
-            lg: 0
-          }}
-        >
-          {/* START: Todo Counter */}
-          <Container
-            display="flex"
-            alignItems="center"
-            justifyContent="center"
-            color="#ffffff"
-            bgImage="https://mini-project-vocasia.vercel.app/dashboard/todo-stats-bg/todo-counter-bg.svg"
-            borderRadius="6px"
-            w={{
-              base: "120px",
-              lg: "140px"
-            }}
-            h="77px">
-            <Heading fontSize="47px" fontWeight="400" mr="8px">
-              {countTodo?.jumlah_data}
-            </Heading>
-            <Text fontSize="12px">
-              To Do <br /> Task Today
-            </Text>
-          </Container>
-          {/* START: Todo Counter */}
+					{/* START: Done Todo Counter */}
+					<Container
+						display='flex'
+						alignItems='center'
+						justifyContent='center'
+						color='#ffffff'
+						bgImage='https://mini-project-vocasia.vercel.app/dashboard/todo-stats-bg/done-counter-bg.svg'
+						borderRadius={{
+							base: '12px',
+							lg: '6px'
+						}}
+						w={{
+							base: '100%',
+							lg: '140px',
+							sm: '100%'
+						}}
+						h={{
+							base: '100%',
+							lg: '77px',
+							sm: '90px'
+						}}
+						bgSize='cover'
+						bgRepeat='no-repeat'>
+						<Heading fontSize='47px' fontWeight='400' mr='8px'>
+							{countDoneTodo?.jumlah_data}
+						</Heading>
+						<Text fontSize='12px'>Done Task</Text>
+					</Container>
+					{/* END: Done Todo Counter */}
 
-          {/* START: Done Todo Counter */}
-          <Container
-            display="flex"
-            alignItems="center"
-            justifyContent="center"
-            color="#ffffff"
-            bgImage="https://mini-project-vocasia.vercel.app/dashboard/todo-stats-bg/done-counter-bg.svg"
-            borderRadius="6px"
-            w={{
-              base: "120px",
-              lg: "140px"
-            }}
-            h="77px">
-            <Heading fontSize="47px" fontWeight="400" mr="8px">
-              {countDoneTodo?.jumlah_data}
-            </Heading>
-            <Text fontSize="12px">Done Task</Text>
-          </Container>
-          {/* END: Done Todo Counter */}
+					{/* START: Overdue Todo Counter */}
+					<Container
+						display='flex'
+						alignItems='center'
+						justifyContent='center'
+						color='#ffffff'
+						bgImage='https://mini-project-vocasia.vercel.app/dashboard/todo-stats-bg/overdue-counter-bg.svg'
+						borderRadius={{
+							base: '12px',
+							lg: '6px'
+						}}
+						w={{
+							base: '100%',
+							lg: '140px',
+							sm: '100%'
+						}}
+						h={{
+							base: '100%',
+							lg: '77px',
+							sm: '90px'
+						}}
+						bgSize='cover'
+						bgRepeat='no-repeat'>
+						<Heading fontSize='47px' fontWeight='400' mr='8px'>
+							{countOverdueTodo?.jumlah_data}
+						</Heading>
+						<Text fontSize='12px'>
+							Overdue <br /> Task
+						</Text>
+					</Container>
+					{/* END: Overdue Todo Counter */}
+				</Stack>
+				{/* END: Todos Counter */}
+			</Flex>
+		</Box>
+	)
+}
 
-          {/* START: Overdue Todo Counter */}
-          <Container
-            display="flex"
-            alignItems="center"
-            justifyContent="center"
-            color="#ffffff"
-            bgImage="https://mini-project-vocasia.vercel.app/dashboard/todo-stats-bg/overdue-counter-bg.svg"
-            borderRadius="6px"
-            w={{
-              base: "120px",
-              lg: "140px"
-            }}
-            h="77px">
-            <Heading fontSize="47px" fontWeight="400" mr="8px">
-              {countOverdueTodo?.jumlah_data}
-            </Heading>
-            <Text fontSize="12px">
-              Overdue <br /> Task
-            </Text>
-          </Container>
-          {/* END: Overdue Todo Counter */}
-        </Stack>
-        {/* END: Todos Counter */}
-      </Flex>
-    </Box>
-  );
-};
-
-export default Header;
+export default Header
--- components/home/Layout/Sidebar.tsx
+++ components/home/Layout/Sidebar.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
 
@@ -6,11 +6,30 @@ import Link from 'next/link'
 import LogoutModal from '../Modal/LogoutModal'
 
 // Styles
-import { Flex, Text, Box, Image, Button } from '@chakra-ui/react'
+import {
+	Flex,
+	Text,
+	Box,
+	Image,
+	Input,
+	Button,
+	Drawer,
+	DrawerBody,
+	DrawerFooter,
+	DrawerHeader,
+	DrawerOverlay,
+	DrawerContent,
+	DrawerCloseButton,
+	useDisclosure
+} from '@chakra-ui/react'
+import { HamburgerIcon } from '@chakra-ui/icons'
 
 const Sidebar = () => {
 	const router = useRouter()
 	const pathName = router.asPath
+
+	const { isOpen, onOpen, onClose } = useDisclosure()
+	const btnRef = useRef()
 
 	const [activeTab, setActiveTab] = useState(pathName)
 	const [tabName, setTabName] = useState(pathName)
@@ -25,173 +44,399 @@ const Sidebar = () => {
 	}, [pathName, tabName])
 
 	return (
-		<Box
-			w='238px'
-			bg='#F8F8FB'
-			borderRight='1px'
-			borderRightColor='#CCCCCC'
-			py='30px'>
-			{/* START: Logo */}
-			<Box mb='56px' px='30px'>
-				<Image
-					src='https://mini-project-vocasia.vercel.app/image/logo.svg'
-					width='147px'
-					height='59px'
-					alt='Logo Vocasia'
+		<>
+			<Button
+				role='group'
+				ref={btnRef}
+				onClick={onOpen}
+				position='fixed'
+				w='40px'
+				h='40px'
+				top='30px'
+				left='20px'
+				display={{
+					base: 'block',
+					lg: 'none',
+					md: 'none',
+					sm: 'block'
+				}}
+				padding='2px'
+				border='2px solid #000000'
+				borderRadius='5px'
+				color='#000000'
+				background='#ffffff'
+				_hover={{
+					background: '#ffffff',
+					borderColor: '#BA181B'
+				}}
+				_active={{
+					background: '#ffffff',
+					borderColor: '#BA181B'
+				}}>
+				<HamburgerIcon
+					w='20px'
+					h='20px'
+					color='#000000'
+					_groupHover={{
+						color: '#BA181B'
+					}}
+					_active={{
+						color: '#BA181B'
+					}}
+				/>
+			</Button>
+			<Drawer
+				isOpen={isOpen}
+				placement='left'
+				onClose={onClose}
+				finalFocusRef={btnRef}
+				blockScrollOnMount
+				closeOnEsc
+				closeOnOverlayClick>
+				<DrawerOverlay />
+				<DrawerContent>
+					<DrawerCloseButton />
+					{/* START: Logo */}
+					<DrawerHeader>
+						<Box mb='56px' px='30px'>
+							<Image
+								src='https://mini-project-vocasia.vercel.app/image/logo.svg'
+								width='147px'
+								height='59px'
+								alt='Logo Vocasia'
+							/>
+						</Box>
+					</DrawerHeader>
+					{/* END: Logo */}
+
+					{/* START: Sidebar Tabs */}
+					<DrawerBody>
+						<Flex direction='column' justify='space-between'>
+							<Box display='flex' flexDirection='column' px='9px' gap='35px'>
+								{/* START: To Do Tab */}
+								<Link href='./home'>
+									<Box
+										id='/home'
+										display='flex'
+										alignItems='center'
+										px='18px'
+										borderLeft={`3px solid ${
+											activeTab === '/home' ? '#BA181B' : 'transparent'
+										}`}
+										_hover={{
+											cursor: 'pointer',
+											borderLeftColor: '#BA181B'
+										}}
+										onClick={event => handleClickActiveTab(event)}>
+										<Image
+											src='https://mini-project-vocasia.vercel.app/icon/todo.svg'
+											width='22px'
+											height='22px'
+											alt='To Do Icon'
+										/>
+										<Text
+											pl='14px'
+											fontSize='14px'
+											letterSpacing='0.025em'
+											fontWeight='500'
+											color={activeTab === '/home' ? '#000000' : '#6A686A'}
+											_hover={{ color: '#000000' }}>
+											To Do
+										</Text>
+									</Box>
+								</Link>
+								{/* END: To Do Tab */}
+
+								{/* START: Done Tab */}
+								<Link href='./done'>
+									<Box
+										id='/done'
+										display='flex'
+										alignItems='center'
+										px='18px'
+										borderLeft={`3px solid ${
+											activeTab === '/done' ? '#BA181B' : 'transparent'
+										}`}
+										_hover={{
+											cursor: 'pointer',
+											borderLeftColor: '#BA181B'
+										}}
+										onClick={event => handleClickActiveTab(event)}>
+										<Image
+											src='https://mini-project-vocasia.vercel.app/icon/done.svg'
+											width='22px'
+											height='22px'
+											alt='Done Icon'
+										/>
+										<Text
+											pl='14px'
+											fontSize='14px'
+											letterSpacing='0.025em'
+											fontWeight='500'
+											color={activeTab === '/done' ? '#000000' : '#6A686A'}
+											_hover={{ color: '#000000' }}>
+											Done
+										</Text>
+									</Box>
+								</Link>
+								{/* END: Done Tab */}
+
+								{/* START: Overdue Tab */}
+								<Link href='./overdue'>
+									<Box
+										id='/overdue'
+										display='flex'
+										alignItems='center'
+										px='18px'
+										borderLeft={`3px solid ${
+											activeTab === '/overdue' ? '#BA181B' : 'transparent'
+										}`}
+										_hover={{ cursor: 'pointer', borderLeftColor: '#BA181B' }}
+										onClick={event => handleClickActiveTab(event)}>
+										<Image
+											src='https://mini-project-vocasia.vercel.app/icon/overdue.svg'
+											width='22px'
+											height='22px'
+											alt='Overdue Icon'
+										/>
+										<Text
+											pl='14px'
+											fontSize='14px'
+											letterSpacing='0.025em'
+											fontWeight='500'
+											color={activeTab === '/overdue' ? '#000000' : '#6A686A'}
+											_hover={{ color: '#000000' }}>
+											Overdue
+										</Text>
+									</Box>
+								</Link>
+								{/* END: Overdue Tab */}
+							</Box>
+						</Flex>
+					</DrawerBody>
+					{/* END: Sidebar Tabs */}
+
+					{/* START: Logout Tab */}
+					<DrawerFooter>
+						<Box
+							role='group'
+							w='100%'
+							pl='20px'
+							display='flex'
+							_hover={{ cursor: 'pointer' }}>
+							<Button
+								background='transparent'
+								onClick={() => setIsLogoutModalOpen(true)}
+								_hover={{ background: 'transparent' }}
+								_active={{ background: 'transparent' }}>
+								<svg
+									xmlns='http://www.w3.org/2000/svg'
+									width='24'
+									height='24'
+									viewBox='0 0 24 24'
+									fill='none'
+									stroke='#BA181B'
+									strokeWidth='2'
+									strokeLinecap='round'
+									strokeLinejoin='round'>
+									<path d='M13 12h9m0 0l-3.333-4M22 12l-3.333 4'></path>
+									<path d='M14 7V5.174a2 2 0 0 0-2.166-1.993l-8 .666A2 2 0 0 0 2 5.84v12.32a2 2 0 0 0 1.834 1.993l8 .667A2 2 0 0 0 14 18.826V17'></path>
+								</svg>
+								<Text
+									pl='14px'
+									fontSize='18px'
+									fontWeight='500'
+									letterSpacing='0.025em'
+									color='#BA181B'
+									_groupHover={{ color: '#9e2427' }}>
+									Logout
+								</Text>
+							</Button>
+						</Box>
+						<LogoutModal
+							isLogoutModalOpen={isLogoutModalOpen}
+							setIsLogoutModalOpen={setIsLogoutModalOpen}
+							onLogoutModalClose={() => setIsLogoutModalOpen(prev => !prev)}
+						/>
+					</DrawerFooter>
+					{/* END: Logout Tab */}
+				</DrawerContent>
+			</Drawer>
+
+			<Box
+				w='238px'
+				bg='#F8F8FB'
+				borderRight='1px'
+				borderRightColor='#CCCCCC'
+				py='30px'
+				display={{
+					base: 'none',
+					lg: 'block',
+					md: 'none',
+					sm: 'none'
+				}}>
+				{/* START: Logo */}
+				<Box mb='56px' px='30px'>
+					<Image
+						src='https://mini-project-vocasia.vercel.app/image/logo.svg'
+						width='147px'
+						height='59px'
+						alt='Logo Vocasia'
+					/>
+				</Box>
+				{/* END: Logo */}
+
+				{/* START: Sidebar Tabs */}
+				<Flex direction='column' h='1024px' justify='space-between'>
+					<Box display='flex' flexDirection='column' px='9px' gap='35px'>
+						{/* START: To Do Tab */}
+						<Link href='./home'>
+							<Box
+								id='/home'
+								display='flex'
+								alignItems='center'
+								px='18px'
+								borderLeft={`3px solid ${
+									activeTab === '/home' ? '#BA181B' : 'transparent'
+								}`}
+								_hover={{
+									cursor: 'pointer',
+									borderLeftColor: '#BA181B'
+								}}
+								onClick={event => handleClickActiveTab(event)}>
+								<Image
+									src='https://mini-project-vocasia.vercel.app/icon/todo.svg'
+									width='22px'
+									height='22px'
+									alt='To Do Icon'
+								/>
+								<Text
+									pl='14px'
+									fontSize='14px'
+									letterSpacing='0.025em'
+									fontWeight='500'
+									color={activeTab === '/home' ? '#000000' : '#6A686A'}
+									_hover={{ color: '#000000' }}>
+									To Do
+								</Text>
+							</Box>
+						</Link>
+						{/* END: To Do Tab */}
+
+						{/* START: Done Tab */}
+						<Link href='./done'>
+							<Box
+								id='/done'
+								display='flex'
+								alignItems='center'
+								px='18px'
+								borderLeft={`3px solid ${
+									activeTab === '/done' ? '#BA181B' : 'transparent'
+								}`}
+								_hover={{
+									cursor: 'pointer',
+									borderLeftColor: '#BA181B'
+								}}
+								onClick={event => handleClickActiveTab(event)}>
+								<Image
+									src='https://mini-project-vocasia.vercel.app/icon/done.svg'
+									width='22px'
+									height='22px'
+									alt='Done Icon'
+								/>
+								<Text
+									pl='14px'
+									fontSize='14px'
+									letterSpacing='0.025em'
+									fontWeight='500'
+									color={activeTab === '/done' ? '#000000' : '#6A686A'}
+									_hover={{ color: '#000000' }}>
+									Done
+								</Text>
+							</Box>
+						</Link>
+						{/* END: Done Tab */}
+
+						{/* START: Overdue Tab */}
+						<Link href='./overdue'>
+							<Box
+								id='/overdue'
+								display='flex'
+								alignItems='center'
+								px='18px'
+								borderLeft={`3px solid ${
+									activeTab === '/overdue' ? '#BA181B' : 'transparent'
+								}`}
+								_hover={{ cursor: 'pointer', borderLeftColor: '#BA181B' }}
+								onClick={event => handleClickActiveTab(event)}>
+								<Image
+									src='https://mini-project-vocasia.vercel.app/icon/overdue.svg'
+									width='22px'
+									height='22px'
+									alt='Overdue Icon'
+								/>
+								<Text
+									pl='14px'
+									fontSize='14px'
+									letterSpacing='0.025em'
+									fontWeight='500'
+									color={activeTab === '/overdue' ? '#000000' : '#6A686A'}
+									_hover={{ color: '#000000' }}>
+									Overdue
+								</Text>
+							</Box>
+						</Link>
+						{/* END: Overdue Tab */}
+					</Box>
+
+					{/* START: Logout Tab */}
+					{/* <Link href='./logout'></Link> */}
+
+					<Box
+						role='group'
+						display='flex'
+						alignItems='center'
+						p='30px'
+						_hover={{ cursor: 'pointer' }}>
+						<Button
+							background='transparent'
+							onClick={() => setIsLogoutModalOpen(true)}
+							_hover={{ background: 'transparent' }}
+							_active={{ background: 'transparent' }}>
+							<svg
+								xmlns='http://www.w3.org/2000/svg'
+								width='24'
+								height='24'
+								viewBox='0 0 24 24'
+								fill='none'
+								stroke='#BA181B'
+								strokeWidth='2'
+								strokeLinecap='round'
+								strokeLinejoin='round'>
+								<path d='M13 12h9m0 0l-3.333-4M22 12l-3.333 4'></path>
+								<path d='M14 7V5.174a2 2 0 0 0-2.166-1.993l-8 .666A2 2 0 0 0 2 5.84v12.32a2 2 0 0 0 1.834 1.993l8 .667A2 2 0 0 0 14 18.826V17'></path>
+							</svg>
+							<Text
+								pl='14px'
+								fontSize='18px'
+								fontWeight='500'
+								letterSpacing='0.025em'
+								color='#BA181B'
+								_groupHover={{ color: '#9e2427' }}>
+								Logout
+							</Text>
+						</Button>
+					</Box>
+					{/* END: Logout Tab */}
+				</Flex>
+				{/* END: Sidebar Tabs */}
+				<LogoutModal
+					isLogoutModalOpen={isLogoutModalOpen}
+					setIsLogoutModalOpen={setIsLogoutModalOpen}
+					onLogoutModalClose={() => setIsLogoutModalOpen(prev => !prev)}
 				/>
 			</Box>
-			{/* END: Logo */}
-
-			{/* START: Sidebar Tabs */}
-			<Flex direction='column' h='1024px' justify='space-between'>
-				<Box display='flex' flexDirection='column' px='9px' gap='35px'>
-					{/* START: To Do Tab */}
-					<Link href='./home'>
-						<Box
-							id='/home'
-							display='flex'
-							alignItems='center'
-							px='18px'
-							borderLeft={`3px solid ${
-								activeTab === '/home' ? '#BA181B' : 'transparent'
-							}`}
-							_hover={{
-								cursor: 'pointer',
-								borderLeftColor: '#BA181B'
-							}}
-							onClick={event => handleClickActiveTab(event)}>
-							<Image
-								src='https://mini-project-vocasia.vercel.app/icon/todo.svg'
-								width='22px'
-								height='22px'
-								alt='To Do Icon'
-							/>
-							<Text
-								pl='14px'
-								fontSize='14px'
-								letterSpacing='0.025em'
-								fontWeight='500'
-								color={activeTab === '/home' ? '#000000' : '#6A686A'}
-								_hover={{ color: '#000000' }}>
-								To Do
-							</Text>
-						</Box>
-					</Link>
-					{/* END: To Do Tab */}
-
-					{/* START: Done Tab */}
-					<Link href='./done'>
-						<Box
-							id='/done'
-							display='flex'
-							alignItems='center'
-							px='18px'
-							borderLeft={`3px solid ${
-								activeTab === '/done' ? '#BA181B' : 'transparent'
-							}`}
-							_hover={{
-								cursor: 'pointer',
-								borderLeftColor: '#BA181B'
-							}}
-							onClick={event => handleClickActiveTab(event)}>
-							<Image
-								src='https://mini-project-vocasia.vercel.app/icon/done.svg'
-								width='22px'
-								height='22px'
-								alt='Done Icon'
-							/>
-							<Text
-								pl='14px'
-								fontSize='14px'
-								letterSpacing='0.025em'
-								fontWeight='500'
-								color={activeTab === '/done' ? '#000000' : '#6A686A'}
-								_hover={{ color: '#000000' }}>
-								Done
-							</Text>
-						</Box>
-					</Link>
-					{/* END: Done Tab */}
-
-					{/* START: Overdue Tab */}
-					<Link href='./overdue'>
-						<Box
-							id='/overdue'
-							display='flex'
-							alignItems='center'
-							px='18px'
-							borderLeft={`3px solid ${
-								activeTab === '/overdue' ? '#BA181B' : 'transparent'
-							}`}
-							_hover={{ cursor: 'pointer', borderLeftColor: '#BA181B' }}
-							onClick={event => handleClickActiveTab(event)}>
-							<Image
-								src='https://mini-project-vocasia.vercel.app/icon/overdue.svg'
-								width='22px'
-								height='22px'
-								alt='Overdue Icon'
-							/>
-							<Text
-								pl='14px'
-								fontSize='14px'
-								letterSpacing='0.025em'
-								fontWeight='500'
-								color={activeTab === '/overdue' ? '#000000' : '#6A686A'}
-								_hover={{ color: '#000000' }}>
-								Overdue
-							</Text>
-						</Box>
-					</Link>
-					{/* END: Overdue Tab */}
-				</Box>
-
-				{/* START: Logout Tab */}
-				{/* <Link href='./logout'></Link> */}
-
-				<Box
-					role='group'
-					display='flex'
-					alignItems='center'
-					p='30px'
-					_hover={{ cursor: 'pointer' }}>
-					<Button
-						background='transparent'
-						onClick={() => setIsLogoutModalOpen(true)}
-						_hover={{ background: 'transparent' }}
-						_active={{ background: 'transparent' }}>
-						<svg
-							xmlns='http://www.w3.org/2000/svg'
-							width='24'
-							height='24'
-							viewBox='0 0 24 24'
-							fill='none'
-							stroke='#BA181B'
-							strokeWidth='2'
-							strokeLinecap='round'
-							strokeLinejoin='round'>
-							<path d='M13 12h9m0 0l-3.333-4M22 12l-3.333 4'></path>
-							<path d='M14 7V5.174a2 2 0 0 0-2.166-1.993l-8 .666A2 2 0 0 0 2 5.84v12.32a2 2 0 0 0 1.834 1.993l8 .667A2 2 0 0 0 14 18.826V17'></path>
-						</svg>
-						<Text
-							pl='14px'
-							fontSize='18px'
-							fontWeight='500'
-							letterSpacing='0.025em'
-							color='#BA181B'
-							_groupHover={{ color: '#9e2427' }}>
-							Logout
-						</Text>
-					</Button>
-				</Box>
-				{/* END: Logout Tab */}
-			</Flex>
-			{/* END: Sidebar Tabs */}
-			<LogoutModal
-				isLogoutModalOpen={isLogoutModalOpen}
-				setIsLogoutModalOpen={setIsLogoutModalOpen}
-				onLogoutModalClose={() => setIsLogoutModalOpen(prev => !prev)}
-			/>
-		</Box>
+		</>
 	)
 }
 
