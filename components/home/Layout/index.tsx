d69903dfff3c63a41746016a463387e3291721a5
MRSA <sanggisda@gmail.com>
2022-09-04 23:37:40 +0700

    fix bug layout and auth component

--- components/home/Layout/index.tsx
+++ components/home/Layout/index.tsx
@@ -1,38 +1,38 @@
-import { useState, useEffect } from 'react'
-import Router from 'next/router'
-import Head from 'next/head'
-import Sidebar from './Sidebar'
-import Header from './Header'
-import { ILayout } from '../../../ts/interface'
+import { useState, useEffect } from "react";
+import Router from "next/router";
+import Head from "next/head";
+import Sidebar from "./Sidebar";
+import Header from "./Header";
+import { ILayout } from "../../../ts/interface";
 
 // Styles
-import { Box } from '@chakra-ui/react'
+import { Box } from "@chakra-ui/react";
 
-const Layout = ({ children, title }: ILayout) => {
-	const [isHaveToken, setIsHaveToken] = useState(false)
+const Layout = ({ children, title }: ILayout): any => {
+  const [isHaveToken, setIsHaveToken] = useState(false);
 
-	useEffect(() => {
-		setIsHaveToken(localStorage.getItem('xtoken') === null)
+  useEffect(() => {
+    setIsHaveToken(localStorage.getItem("xtoken") === null);
 
-		if (isHaveToken !== false) {
-			Router.push('/')
-		}
-	}, [isHaveToken])
+    if (isHaveToken !== false) {
+      Router.push("/");
+    }
+  }, [isHaveToken]);
 
-	return (
-		isHaveToken === false && (
-			<Box bg='white' color='black' display='flex' maxW='100%' minH='1024px'>
-				<Head>
-					<title>{title}</title>
-				</Head>
-				<Sidebar />
-				<Box w='100%'>
-					<Header />
-					{children}
-				</Box>
-			</Box>
-		)
-	)
-}
+  return (
+    isHaveToken === false && (
+      <Box bg="white" color="black" display="flex" maxW="100%" minH="1024px">
+        <Head>
+          <title>{title}</title>
+        </Head>
+        <Sidebar />
+        <Box w="100%">
+          <Header />
+          {children}
+        </Box>
+      </Box>
+    )
+  );
+};
 
-export default Layout
+export default Layout;
--- pages/auth/index.tsx
+++ pages/auth/index.tsx
@@ -1,11 +1,11 @@
-import Head from 'next/head'
-import { Grid } from '@chakra-ui/react'
+import Head from "next/head";
+import { Grid } from "@chakra-ui/react";
 
-import LayoutAuth from '../../components/auth/layout'
-import Login from '../../components/auth/Login'
+import LayoutAuth from "../../components/auth/layout";
+import Login from "../../components/auth/Login";
 // import authPage from "../middleware/authorizationPage";
-import { useState, useEffect } from 'react'
-import Router from 'next/router'
+import { useState, useEffect } from "react";
+import Router from "next/router";
 
 // export async function getServerSideProps(ctx: any) {
 //   const token = ctx.req?.cookies?.ci_session;
@@ -21,31 +21,31 @@ import Router from 'next/router'
 //   };
 // }
 
-const Index = () => {
-	const [isHaveToken, setIsHaveToken] = useState(false)
+const Index = (): any => {
+  const [isHaveToken, setIsHaveToken] = useState(false);
 
-	useEffect(() => {
-		setIsHaveToken(localStorage.getItem('xtoken') !== null)
+  useEffect(() => {
+    setIsHaveToken(localStorage.getItem("xtoken") !== null);
 
-		if (isHaveToken) {
-			Router.push('/home')
-		}
-	}, [isHaveToken])
+    if (isHaveToken) {
+      Router.push("/home");
+    }
+  }, [isHaveToken]);
 
-	return (
-		isHaveToken === false && (
-			<>
-				<Head>
-					<title>Masuk</title>
-				</Head>
+  return (
+    isHaveToken === false && (
+      <>
+        <Head>
+          <title>Masuk</title>
+        </Head>
 
-				<Grid bg='#F8F8FB' templateColumns={{ base: '1fr', md: '1fr 1fr' }}>
-					<LayoutAuth />
-					<Login />
-				</Grid>
-			</>
-		)
-	)
-}
+        <Grid bg="#F8F8FB" templateColumns={{ base: "1fr", md: "1fr 1fr" }}>
+          <LayoutAuth />
+          <Login />
+        </Grid>
+      </>
+    )
+  );
+};
 
-export default Index
+export default Index;
