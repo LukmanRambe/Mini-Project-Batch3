e5ebc54906e73d1c7b17dba8287490e9d422333c
MRSA <sanggisda@gmail.com>
2022-09-05 14:20:45 +0700

    add skeleton, add toast login

--- components/auth/Login.tsx
+++ components/auth/Login.tsx
@@ -1,314 +1,278 @@
-import { useState } from 'react'
-import NextLink from 'next/link'
-import Router from 'next/router'
-import Cookies from 'universal-cookie'
+import { useState } from "react";
+import NextLink from "next/link";
+import Router from "next/router";
+import Cookies from "universal-cookie";
+import axios from "axios";
 import {
-	Button,
-	GridItem,
-	VStack,
-	Box,
-	Heading,
-	Text,
-	Input,
-	Flex,
-	InputRightElement,
-	InputGroup,
-	Link,
-	Container,
-	Center,
-	FormControl,
-	Alert,
-	AlertIcon,
-	AlertTitle,
-	AlertDescription,
-	CloseButton,
-	Spinner
-} from '@chakra-ui/react'
-import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons'
-import { useStore } from '../../store/useStore'
+  Button,
+  GridItem,
+  VStack,
+  Box,
+  Heading,
+  Text,
+  Input,
+  Flex,
+  InputRightElement,
+  InputGroup,
+  Link,
+  Container,
+  Center,
+  FormControl,
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
+  CloseButton,
+  Spinner,
+  useToast,
+} from "@chakra-ui/react";
+import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
+import { useStore } from "../../store/useStore";
 
 const Login = () => {
-	const { login } = useStore()
-	// Input User
-	const [email, setEmail] = useState('')
-	const [password, setPassword] = useState('')
+  const { login } = useStore();
+  const toast = useToast();
+  // Input User
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-	// Error
-	const [error, setError] = useState('')
-	const [errorPassword, setErrorPassword] = useState('')
-	const [errorEmail, setErrorEmail] = useState('')
+  const [show, setShow] = useState<boolean>(false);
+  const [loading, setLoading] = useState(false);
 
-	// code status Success login
-	const [responseCode, setResponCode] = useState<number | null>(null)
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
 
-	const [show, setShow] = useState<boolean>(false)
-	const [loading, setLoading] = useState(false)
+  const handlePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
 
-	const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-		setEmail(event.target.value)
-	}
+  function validatePassword(): boolean {
+    var validate = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/;
+    if (validate.test(password) === false) {
+      toast({
+        position: "top",
+        title: "Gagal",
+        description: "Password minimal 8 Character, Satu Huruf Besar dan Angka",
+        status: "error",
+        duration: 3000,
+        isClosable: true,
+      });
+      return false;
+    } else {
+      return true;
+    }
+  }
 
-	const handlePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
-		setPassword(event.target.value)
-	}
+  function validateEmail(): boolean {
+    if (email === "") {
+      toast({
+        position: "top",
+        title: "Gagal",
+        description: "Email Harus Diisi",
+        status: "error",
+        duration: 3000,
+        isClosable: true,
+      });
+      return false;
+    } else {
+      return true;
+    }
+  }
 
-	function validatePassword(): boolean {
-		var validate = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/
-		if (validate.test(password) === false) {
-			setErrorPassword(
-				'Password minimal 8 Character, Satu Huruf Besar dan Angka'
-			)
-			return false
-		} else {
-			setErrorPassword('')
-			return true
-		}
-	}
+  const fetcher = async () => {
+    const cookies = new Cookies();
+    setLoading(true);
+    await axios
+      .post(
+        "https://nouky.xyz/b3/login",
+        {
+          email: email,
+          password: password,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json; charset=utf-8",
+          },
+        }
+      )
+      .then((result) => {
+        console.log(result.data.data);
+        if (result.data.code === 200) {
+          toast({
+            position: "top",
+            title: "Berhasil",
+            description: result.data.message,
+            status: "success",
+            duration: 3000,
+            isClosable: true,
+          });
+          // CookieAdd untuk logout
+          cookies.set("ci_session", result.data.data.token, {
+            path: "/",
+            domain:
+              process.env.NODE_ENV === "development"
+                ? "localhost"
+                : "mini-project-batch3.vercel.app",
+          });
+          // TokenJWT
+          let dataToken = JSON.stringify(result.data.data.token);
+          localStorage.setItem("xtoken", JSON.parse(dataToken));
+          // nameUser
+          let Name = JSON.stringify(result.data.data.name);
+          localStorage.setItem("name", JSON.parse(Name));
+          // auth;
+          setTimeout(() => {
+            login();
+            Router.push("/home");
+          }, 1000);
+          setLoading(false);
+        }
+      })
+      .catch((err) => {
+        setLoading(true);
+        console.log(err);
+        if (err.code === "ECONNABORTED") {
+          toast({
+            position: "top",
+            title: "Gagal",
+            description:
+              "Tidak dapat menjangkau Server, periksa koneksi Anda dan ulangi beberapa saat lagi.",
+            status: "error",
+            duration: 3000,
+            isClosable: true,
+          });
+          setLoading(false);
+        } else if (err.response) {
+          toast({
+            position: "top",
+            title: "Gagal",
+            description: err.response.data.messages.error,
+            status: "error",
+            duration: 3000,
+            isClosable: true,
+          });
+          setLoading(false);
+        }
+      });
+  };
 
-	function validateEmail(): boolean {
-		if (email === '') {
-			setErrorEmail('Email Harus Diisi')
-			return false
-		} else {
-			setErrorEmail('')
-			return true
-		}
-	}
-	// close Alert
-	const onClose = () => {
-		setError('')
-		setErrorPassword('')
-		setErrorEmail('')
-		setResponCode(null)
-	}
+  const handleMasuk = (event: React.FormEvent) => {
+    event.preventDefault();
+    if (validateEmail()) {
+      if (validatePassword()) {
+        // fetching API and Login
+        fetcher();
+      }
+    }
+  };
 
-	const fetcher = async () => {
-		const cookies = new Cookies()
-		try {
-			setLoading(true)
-			const response = await fetch('https://nouky.xyz/b3/login', {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json; charset=utf-8'
-				},
-				body: JSON.stringify({
-					email: email,
-					password: password
-				})
-			})
+  // show type password
+  const handleShow = () => {
+    setShow(!show);
+  };
 
-			const responseData = await response.json()
-			if (responseData.code === 200) {
-				// CookieAdd untuk logout
-				cookies.set('ci_session', responseData.data.token, {
-					path: '/',
-					domain:
-						process.env.NODE_ENV === 'development'
-							? 'localhost'
-							: 'mini-project-batch3.vercel.app'
-				})
-				// TokenJWT
-				let dataToken = JSON.stringify(responseData.data.token)
-				localStorage.setItem('xtoken', JSON.parse(dataToken))
-				// nameUser
-				let Name = JSON.stringify(responseData.data.name)
-				localStorage.setItem('name', JSON.parse(Name))
-				// auth(JSON.parse(dataToken));
-				setResponCode(responseData.code)
-				// auth;
-				setTimeout(() => {
-					login()
-					Router.push('/home')
-				}, 1000)
-				setLoading(false)
-			} else {
-				setLoading(false)
-				setError(responseData.messages.error)
-			}
-		} catch (err) {
-			setLoading(true)
-			setTimeout(() => {
-				setLoading(false)
-				setError('Server Terjadi Masalah')
-			}, 500)
-			setTimeout(() => {
-				setError('')
-			}, 2000)
-		}
-	}
+  return (
+    <GridItem>
+      <VStack
+        w="full"
+        minH="100vh"
+        display="flex"
+        justifyContent="center"
+        alignItems="center">
+        <Container>
+          <Box textAlign="center" mb={7}>
+            <Heading as="h1" size="md" mt={2}>
+              Masuk
+            </Heading>
+          </Box>
+          <form onSubmit={(e) => handleMasuk(e)}>
+            <FormControl isRequired>
+              <Flex gap={1} marginTop={4} marginBottom={2}>
+                <Text fontSize="md">Email </Text>
+                <Text fontSize="md" color="red">
+                  {" "}
+                  *
+                </Text>
+              </Flex>
+              <Input
+                type="text"
+                py="13px"
+                px="20px"
+                borderRadius="12px"
+                bg="white"
+                onChange={(e) => handleEmailChange(e)}
+                placeholder="Masukkan Email"
+                required
+              />
+              <Flex gap={1} marginTop={4} marginBottom={2}>
+                <Text fontSize="md">Password </Text>
+                <Text fontSize="md" color="red">
+                  {" "}
+                  *
+                </Text>
+              </Flex>
+              <InputGroup size="md">
+                <Input
+                  type={show ? "text" : "password"}
+                  py="13px"
+                  px="20px"
+                  borderRadius="12px"
+                  bg="white"
+                  placeholder="Password"
+                  onChange={(e) => handlePassword(e)}
+                  required
+                />
+                <InputRightElement>
+                  <Button
+                    h="full"
+                    bg="transparent"
+                    size="md"
+                    borderRightRadius="12px"
+                    borderLeftRadius="0"
+                    onClick={handleShow}>
+                    {show ? <ViewIcon /> : <ViewOffIcon />}
+                  </Button>
+                </InputRightElement>
+              </InputGroup>
+              <Flex justify="flex-end" mt="15px">
+                <NextLink href="/auth/forget" passHref>
+                  <Link color="primary">Lupa Password?</Link>
+                </NextLink>
+              </Flex>
+              <Button
+                type="submit"
+                size="md"
+                bg="#BA181B"
+                _hover={{ bg: "#9e2427" }}
+                color="#fff"
+                mt={7}
+                w="full"
+                _active={{
+                  bg: "#9e2427",
+                  transform: "scale(0.98)",
+                }}>
+                {!loading && <Text>Masuk</Text>}
+                {loading && <Spinner></Spinner>}
+              </Button>
+            </FormControl>
+          </form>
+          <Center>
+            <Flex mt="20px">
+              <Text color="#737373" fontSize="sm">
+                Belum Punya Akun?
+                <NextLink href="/auth/register" passHref>
+                  <Link color="primary" fontWeight="bold" ml="1">
+                    Daftar
+                  </Link>
+                </NextLink>
+              </Text>
+            </Flex>
+          </Center>
+        </Container>
+      </VStack>
+    </GridItem>
+  );
+};
 
-	const handleMasuk = (event: React.FormEvent) => {
-		event.preventDefault()
-		if (validateEmail()) {
-			if (validatePassword()) {
-				// fetching API and Login
-				fetcher()
-			}
-		}
-	}
-
-	// show type password
-	const handleShow = () => {
-		setShow(!show)
-	}
-
-	return (
-		<GridItem>
-			<VStack
-				w='full'
-				minH='100vh'
-				display='flex'
-				justifyContent='center'
-				alignItems='center'>
-				<Container>
-					{/* Alert */}
-					{error || errorEmail || errorPassword ? (
-						<Alert
-							status='error'
-							position='absolute'
-							w='auto'
-							top='2'
-							left='50%'
-							transform='translateX(-50%)'
-							borderRadius='md'
-							color='white'
-							bg='red.400'>
-							<AlertIcon color='white' />
-							<Box pr={10} pl={2}>
-								<AlertTitle>Error!</AlertTitle>
-								<AlertDescription>
-									{error || errorEmail || errorPassword}
-								</AlertDescription>
-							</Box>
-							<CloseButton
-								alignSelf='flex-start'
-								position='absolute'
-								right={2}
-								top={2}
-								onClick={onClose}
-							/>
-						</Alert>
-					) : responseCode === 200 ? (
-						<Alert
-							status='success'
-							position='absolute'
-							w='auto'
-							top='2'
-							left='50%'
-							transform='translateX(-50%)'
-							borderRadius='md'
-							color='white'
-							bg='green.400'>
-							<AlertIcon color='white' />
-							<Box pr={10} pl={2}>
-								<AlertTitle>Login Berhasil!</AlertTitle>
-								<AlertDescription>
-									Kamu Akan Dialihkan Ke Dashboard
-								</AlertDescription>
-							</Box>
-							<CloseButton
-								alignSelf='flex-start'
-								position='absolute'
-								right={2}
-								top={2}
-								onClick={onClose}
-							/>
-						</Alert>
-					) : (
-						''
-					)}
-					<Box textAlign='center' mb={7}>
-						<Heading as='h1' size='md' mt={2}>
-							Masuk
-						</Heading>
-					</Box>
-					<form onSubmit={e => handleMasuk(e)}>
-						<FormControl isRequired>
-							<Flex gap={1} marginTop={4} marginBottom={2}>
-								<Text fontSize='md'>Email </Text>
-								<Text fontSize='md' color='red'>
-									{' '}
-									*
-								</Text>
-							</Flex>
-							<Input
-								type='text'
-								py='13px'
-								px='20px'
-								borderRadius='12px'
-								bg='white'
-								onChange={e => handleEmailChange(e)}
-								placeholder='Masukkan Email'
-								required
-							/>
-							<Flex gap={1} marginTop={4} marginBottom={2}>
-								<Text fontSize='md'>Password </Text>
-								<Text fontSize='md' color='red'>
-									{' '}
-									*
-								</Text>
-							</Flex>
-							<InputGroup size='md'>
-								<Input
-									type={show ? 'text' : 'password'}
-									py='13px'
-									px='20px'
-									borderRadius='12px'
-									bg='white'
-									placeholder='Password'
-									onChange={e => handlePassword(e)}
-									required
-								/>
-								<InputRightElement>
-									<Button
-										h='full'
-										bg='transparent'
-										size='md'
-										borderRightRadius='12px'
-										borderLeftRadius='0'
-										onClick={handleShow}>
-										{show ? <ViewIcon /> : <ViewOffIcon />}
-									</Button>
-								</InputRightElement>
-							</InputGroup>
-							<Flex justify='flex-end' mt='15px'>
-								<NextLink href='/auth/forget' passHref>
-									<Link color='primary'>Lupa Password?</Link>
-								</NextLink>
-							</Flex>
-							<Button
-								type='submit'
-								size='md'
-								bg='#BA181B'
-								_hover={{ bg: '#9e2427' }}
-								color='#fff'
-								mt={7}
-								w='full'
-								_active={{
-									bg: '#9e2427',
-									transform: 'scale(0.98)'
-								}}>
-								{!loading && <Text>Masuk</Text>}
-								{loading && <Spinner></Spinner>}
-							</Button>
-						</FormControl>
-					</form>
-					<Center>
-						<Flex mt='20px'>
-							<Text color='#737373' fontSize='sm'>
-								Belum Punya Akun?
-								<NextLink href='/auth/register' passHref>
-									<Link color='primary' fontWeight='bold' ml='1'>
-										Daftar
-									</Link>
-								</NextLink>
-							</Text>
-						</Flex>
-					</Center>
-				</Container>
-			</VStack>
-		</GridItem>
-	)
-}
-
-export default Login
+export default Login;
--- components/home/Modal/LogoutModal.tsx
+++ components/home/Modal/LogoutModal.tsx
@@ -1,115 +1,115 @@
 // Styles
 import {
-	Modal,
-	ModalOverlay,
-	ModalContent,
-	ModalHeader,
-	ModalFooter,
-	ModalBody,
-	ModalCloseButton,
-	Button,
-	useToast
-} from '@chakra-ui/react'
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  useToast,
+} from "@chakra-ui/react";
 
-import { ILogoutModal } from '../../../ts/interface'
-import { useRouter } from 'next/router'
-import Cookies from 'universal-cookie'
-import { useStore } from '../../../store/useStore'
+import { ILogoutModal } from "../../../ts/interface";
+import { useRouter } from "next/router";
+import Cookies from "universal-cookie";
+import { useStore } from "../../../store/useStore";
 
 const LogoutModal = ({
-	isLogoutModalOpen,
-	setIsLogoutModalOpen,
-	onLogoutModalClose
+  isLogoutModalOpen,
+  setIsLogoutModalOpen,
+  onLogoutModalClose,
 }: ILogoutModal) => {
-	const router = useRouter()
-	const toast = useToast()
-	const { logout } = useStore()
-	const cookies = new Cookies()
-	const handleLogout = async () => {
-		setIsLogoutModalOpen(false)
+  const router = useRouter();
+  const toast = useToast();
+  const { logout } = useStore();
+  const cookies = new Cookies();
+  const handleLogout = async () => {
+    setIsLogoutModalOpen(false);
 
-		try {
-			await fetch('https://nouky.xyz/b3/logout', {
-				method: 'DELETE',
-				headers: {
-					Authorization: 'Bearer ' + localStorage.getItem('xtoken')
-				}
-			})
-			toast({
-				position: 'top',
-				title: 'Berhasil',
-				description: 'Logout berhasil.',
-				status: 'success',
-				duration: 1000,
-				isClosable: true
-			})
-			cookies.remove('ci_session', {
-				path: '/',
-				domain:
-					process.env.NODE_ENV === 'development'
-						? 'localhost'
-						: 'mini-project-batch3.vercel.app'
-			})
-			localStorage.clear()
-			setTimeout(() => {
-				logout()
-				router.push('/')
-			}, 1000)
-		} catch (err) {
-			console.log(err)
-		}
-	}
-	return (
-		<Modal
-			isOpen={isLogoutModalOpen}
-			onClose={onLogoutModalClose}
-			blockScrollOnMount={false}>
-			<ModalOverlay />
-			<ModalContent
-				alignSelf='center'
-				maxWidth={{
-					base: '290px',
-					md: '448px'
-				}}>
-				<ModalHeader textAlign='center' color='#BA181B'>
-					Logout
-				</ModalHeader>
+    try {
+      await fetch("https://nouky.xyz/b3/logout", {
+        method: "DELETE",
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("xtoken"),
+        },
+      });
+      toast({
+        position: "top",
+        title: "Berhasil",
+        description: "Logout berhasil.",
+        status: "success",
+        duration: 1000,
+        isClosable: true,
+      });
+      cookies.remove("ci_session", {
+        path: "/",
+        domain:
+          process.env.NODE_ENV === "development"
+            ? "localhost"
+            : "mini-project-batch3.vercel.app",
+      });
+      localStorage.clear();
+      setTimeout(() => {
+        logout();
+        router.push("/");
+      }, 1000);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  return (
+    <Modal
+      isOpen={isLogoutModalOpen}
+      onClose={onLogoutModalClose}
+      blockScrollOnMount={false}>
+      <ModalOverlay />
+      <ModalContent
+        alignSelf="center"
+        maxWidth={{
+          base: "290px",
+          md: "448px",
+        }}>
+        <ModalHeader textAlign="center" color="#BA181B">
+          Logout
+        </ModalHeader>
 
-				<ModalCloseButton />
-				<ModalBody textAlign='center'>
-					Apakah Anda yakin ingin keluar?
-				</ModalBody>
-				<ModalFooter justifyContent='center' pt={10} pb={5}>
-					<Button
-						bg='transparent'
-						size='lg'
-						_hover={{ color: '#9e2427' }}
-						color='#BA181B'
-						_active={{
-							bg: 'transparent',
-							transform: 'scale(0.98)'
-						}}
-						w={175}
-						onClick={() => setIsLogoutModalOpen(prevState => !prevState)}>
-						Batalkan
-					</Button>
-					<Button
-						onClick={handleLogout}
-						bg='#BA181B'
-						size='lg'
-						_hover={{ bg: '#9e2427' }}
-						color='#fff'
-						_active={{
-							bg: '#9e2427',
-							transform: 'scale(0.98)'
-						}}
-						w={175}>
-						Keluar
-					</Button>
-				</ModalFooter>
-			</ModalContent>
-		</Modal>
-	)
-}
+        <ModalCloseButton />
+        <ModalBody textAlign="center">
+          Apakah Anda yakin ingin keluar?
+        </ModalBody>
+        <ModalFooter justifyContent="center" pt={10} pb={5}>
+          <Button
+            bg="transparent"
+            size="lg"
+            _hover={{ color: "#9e2427" }}
+            color="#BA181B"
+            _active={{
+              bg: "transparent",
+              transform: "scale(0.98)",
+            }}
+            w={175}
+            onClick={() => setIsLogoutModalOpen((prevState) => !prevState)}>
+            Batalkan
+          </Button>
+          <Button
+            onClick={handleLogout}
+            bg="#BA181B"
+            size="lg"
+            _hover={{ bg: "#9e2427" }}
+            color="#fff"
+            _active={{
+              bg: "#9e2427",
+              transform: "scale(0.98)",
+            }}
+            w={175}>
+            Keluar
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
 
-export default LogoutModal
+export default LogoutModal;
